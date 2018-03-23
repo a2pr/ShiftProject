35cf82bf28c102a5ccd4003bf6955816f1d5b69e
A2PR <a2payema@gmail.com>
2018-03-22 23:53:55 -0400

    Configuring Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MaterialModule} from './material/material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
+//Components
 import { AppComponent } from './app.component';
 
+//Services
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserAnimationsModule
   ],
   imports: [
     BrowserModule
