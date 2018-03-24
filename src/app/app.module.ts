b4806d44fdd1531ad05a1f0eea6817c6afce51fb
A2PR <a2payema@gmail.com>
2018-03-24 00:56:57 -0400

    some view roudh sketching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MaterialModule} from './material/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 //Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 //Services
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrowserAnimationsModule
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   imports: [
-    CommonModule
-  ], exports:[],
+    CommonModule,
+    MatCardModule
+  ], exports:[
+    MatCardModule,
+  MatButtonModule],
   declarations: []
 })
 export class MaterialModule { }
